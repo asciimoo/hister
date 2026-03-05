2ab32b93b6315a6536a19b21bbcfedcef2d80ce0
FlameFlag <github@flameflag.dev>
2026-03-05 18:23:34 +0200

    [enh] add Prettier and EditorConfig across all workspaces

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+};
